85d72b57d77ed6e2d927eea180ba87f86485db99
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2022-12-09 20:37:06 -0300

    filter y order (actions y reducer)

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_FAVORITES, DELETE_FAVORITES } from "./actions";
+import { ADD_FAVORITES, DELETE_FAVORITES, FILTER, ORDER } from "./actions";
 
 const initialState = {
   myFavorites: [],
@@ -10,6 +10,24 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         myFavorites: [...state.myFavorites, action.payload],
+      };
+    case FILTER:
+      return {
+        ...state.allCharacters,
+        myFavorites: state.allCharacters.filter(
+          (e) => e.genere == action.payload
+        ),
+      };
+    case ORDER:
+      return {
+        ...state.allCharacters,
+        myFavorites: state.allCharacters.sort(function (a, b) {
+          if (action.payload === "Ascendente") {
+            return a - b;
+          } else if (action.payload === "Descendente") {
+            return b - a;
+          }
+        }),
       };
     case DELETE_FAVORITES:
       return {
